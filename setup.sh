f884b1735854ff45d5af0d7314483990c6af276e
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-05-27 15:43:46 +0530

    feat: add setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-./dots/setup.sh
+cd "$PWD/dots" || exit
+./setup.sh
+cd ..
 ln -sfnv "$PWD/.yabairc" "$HOME/.yabairc"
 ln -sfnv "$PWD/.skhdrc" "$HOME/.skhdrc"
 ln -sfnv "$PWD/dots/.config/lazygit/config.yml" "$HOME/Library/Application Support/lazygit/"
