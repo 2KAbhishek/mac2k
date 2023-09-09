098fb4e9c0fc2440aed1e1eb8ca8ebf79cbfb6b3
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-09-09 05:52:52 +0530

    refactor: move files to config

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@
 cd "$PWD/dots" || exit
 ./setup.sh
 cd ..
-ln -sfnv "$PWD/.yabairc" "$HOME/.yabairc"
-ln -sfnv "$PWD/.skhdrc" "$HOME/.skhdrc"
-ln -sfnv "$PWD/dots/.config/lazygit/config.yml" "$HOME/Library/Application Support/lazygit/"
+ln -sfnv "$PWD/config/.yabairc" "$HOME/.yabairc"
+ln -sfnv "$PWD/config/.skhdrc" "$HOME/.skhdrc"
+ln -sfnv "$PWD/dots/config/lazygit/config.yml" "$HOME/Library/Application Support/lazygit/"
