562e0aa56f24f217f19317e3f85d96ad089e8455
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-11-19 21:26:59 +0530

    feat: faster setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-git submodule update --init --recursive --remote
+function setup {
+    cd "$PWD/dots2k" && ./setup.sh && cd .. || exit 1
+}
 
-cd "$PWD/dots2k" && ./setup.sh && cd .. || exit 1
-
-ln -sfn "$PWD/config/.yabairc" "$HOME/.yabairc"
-ln -sfn "$PWD/config/.skhdrc" "$HOME/.skhdrc"
-ln -sfn "$PWD/config/iterm.sh" "$HOME/.local/bin/iterm"
-ln -sfn "$PWD/dots2k/config/lazygit/config.yml" "$HOME/Library/Application Support/lazygit/"
+if [ -f "$PWD/dots2k/setup.sh" ]; then
+    setup
+else
+    git submodule update --init --recursive --remote && setup
+fi
