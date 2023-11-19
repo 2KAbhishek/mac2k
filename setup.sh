e894967ad3a73ca757ad67097527302ce824ad68
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-11-19 19:12:25 +0530

    feat: silent symlinks

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ git submodule update --init --recursive --remote
 
 cd "$PWD/dots2k" && ./setup.sh && cd .. || exit 1
 
-ln -sfnv "$PWD/config/.yabairc" "$HOME/.yabairc"
-ln -sfnv "$PWD/config/.skhdrc" "$HOME/.skhdrc"
-ln -sfnv "$PWD/config/iterm.sh" "$HOME/.local/bin/iterm"
-ln -sfnv "$PWD/dots2k/config/lazygit/config.yml" "$HOME/Library/Application Support/lazygit/"
+ln -sfn "$PWD/config/.yabairc" "$HOME/.yabairc"
+ln -sfn "$PWD/config/.skhdrc" "$HOME/.skhdrc"
+ln -sfn "$PWD/config/iterm.sh" "$HOME/.local/bin/iterm"
+ln -sfn "$PWD/dots2k/config/lazygit/config.yml" "$HOME/Library/Application Support/lazygit/"
