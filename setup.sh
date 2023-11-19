33932ca1c358fce9659b5892a5061fd1aa34c0cf
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-11-19 19:09:06 +0530

    feat: add submodule to setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-cd "$PWD/dots2k" || exit
-./setup.sh
-cd ..
+git submodule update --init --recursive --remote
+
+cd "$PWD/dots2k" && ./setup.sh && cd .. || exit 1
+
 ln -sfnv "$PWD/config/.yabairc" "$HOME/.yabairc"
 ln -sfnv "$PWD/config/.skhdrc" "$HOME/.skhdrc"
 ln -sfnv "$PWD/config/iterm.sh" "$HOME/.local/bin/iterm"
