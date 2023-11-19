404ee959cc58b8c6886004beb3ad2e02ef0fed0b
Abhishek Keshri <abhishek@Abhisheks-Mac-mini.local>
2023-11-19 12:08:25 +0530

    feat: update setup

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd "$PWD/dots" || exit
+cd "$PWD/dots2k" || exit
 ./setup.sh
 cd ..
 ln -sfnv "$PWD/config/.yabairc" "$HOME/.yabairc"
 ln -sfnv "$PWD/config/.skhdrc" "$HOME/.skhdrc"
 ln -sfnv "$PWD/config/iterm.sh" "$HOME/.local/bin/iterm"
-ln -sfnv "$PWD/dots/config/lazygit/config.yml" "$HOME/Library/Application Support/lazygit/"
+ln -sfnv "$PWD/dots2k/config/lazygit/config.yml" "$HOME/Library/Application Support/lazygit/"
